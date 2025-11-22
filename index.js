f0cf908645af63d5e2d315e7f773f0244f797918
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2025-11-22 13:06:52 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://CleanSphereDB:r6b6rfuwdxXQwiiz@cluster0.hgbitkj.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('CleanSphereDB')
+    const issuesCollection = db.collection('issues')
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('server is running well (CleanSphere)')
