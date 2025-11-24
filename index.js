f21aa81e6990660d2eeeb432932438aa9b49f1e2
Rafiul Islam Jisan <rafiuljisan2005@gmail.com>
2025-11-24 23:55:44 +0600

    added get API to fetch single data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.port || 3000;
 
@@ -10,31 +10,40 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgbitkj.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    await client.connect();
-    const db = client.db('CleanSphereDB')
-    const issuesCollection = db.collection('issues');
+    try {
+        await client.connect();
+        const db = client.db('CleanSphereDB')
+        const issuesCollection = db.collection('issues');
 
-    app.get('/issues', async (req, res) => {
-        const result = await issuesCollection.find().toArray();
-        res.send(result)
-    })
+        app.get('/issues', async (req, res) => {
+            const result = await issuesCollection.find().toArray();
+            res.send(result)
+        });
+
+        app.get('/issues/:id', async (req, res) => {
+            const { id } = req.params;
+            const objectId = new ObjectId(id)
+
+            const result = await issuesCollection.findOne({_id: objectId})
+
+                res.send(result)
+        })
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
 
-  }
+    }
 }
 run().catch(console.dir);
 
